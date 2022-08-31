20a7ec16e34f82bd82a1144192c6fd93cb790b9c
KhitraNazariy <khitranazariy19@gmail.com>
2022-08-31 16:18:01 +0300

    popup some fixes

--- src/components/CastPerson/CastPerson.tsx
+++ src/components/CastPerson/CastPerson.tsx
@@ -1,10 +1,15 @@
-import React, {ChangeEvent, FC, FormEvent, InputHTMLAttributes, useState} from 'react';
+import React, {ChangeEvent, FC, useEffect, useRef, useState} from 'react';
 import {Link} from "react-router-dom";
+import {AiFillStar} from "react-icons/ai";
 
 import scss from './CastPerson.module.scss';
 import {ICombineCreditsCast} from "../../types/ICombinedCredits";
 import {getYear} from "../../utils/getYear";
 import {URL_IMG} from "../../configs";
+
+type PopupClick = MouseEvent & {
+    path: Node[];
+};
 
 const CastPerson: FC<ICombineCreditsCast> = (
     {
@@ -15,11 +20,29 @@ const CastPerson: FC<ICombineCreditsCast> = (
         first_air_date,
         id,
         media_type,
-        poster_path
+        poster_path,
+        overview,
+        vote_average
     }
 ) => {
 
     const [isChecked, setIsChecked] = useState(false);
+    const detailsRef = useRef<HTMLDivElement>(null);
+    
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+          const _event = event as PopupClick
+
+            if (detailsRef.current && !_event.path.includes(detailsRef.current)) {
+                setIsChecked(false)
+            }
+        };
+
+        document.body.addEventListener('click', handleClickOutside);
+
+        return () => document.body.removeEventListener('click', handleClickOutside)
+
+    },[])
 
     const getDate = (releaseDate: string | undefined, firstAirDate: string | undefined): string | undefined => {
         if (!releaseDate) {
@@ -32,13 +55,20 @@ const CastPerson: FC<ICombineCreditsCast> = (
     }
 
     return (
-        <div className={scss.block}>
-            <div style={{display: isChecked ? 'block' : 'none'}} className={scss.block_popup}>
-                <img src={`${URL_IMG}${poster_path}`} alt={`${name}${title}`}/>
+        <div ref={detailsRef} className={scss.block}>
+            <div style={{display: isChecked ? 'flex' : 'none'}} className={scss.block_popup}>
+                <Link to={`/${media_type}/${id.toString()}`}>
+                    <img src={`${URL_IMG}${poster_path}`} alt={`${name}${title}`}/>
+                </Link>
                 <div className={scss.block_popup_details}>
-                    <h3>{name}{title}</h3>
+                    <div className={scss.block_popup_details_title}>
+                        <Link to={`/${media_type}/${id.toString()}`}>
+                            <h3>{name}{title}</h3>
+                        </Link>
+                        <p><AiFillStar/>{vote_average}</p>
+                    </div>
+                    <p>{overview ? overview : 'Опис відсутній'}</p>
                 </div>
-                <button onClick={() => setIsChecked(!isChecked)}>close</button>
             </div>
             <div className={scss.block_year}>{getYear(getDate(release_date, first_air_date))}</div>
             <input type="radio" onChange={onChange} checked={isChecked}/>
